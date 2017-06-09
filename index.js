906ab5b26784a8f4d34678056f362495d401dbcb
Victor Goya <goya.victor@gmail.com>
2017-06-09 14:33:41 +0200

    return 200 on /

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var app = express();
 var scale = require('./express-sharp');
 
 var options = { baseHost: process.env.BASE_HOST || 's3.amazonaws.com' };
+app.get('/', function (req, res) {
+  res.json({ok:true})
+});
+
 app.use('/resize', scale(options));
 app.use('/resize/:name', scale(options));
 
